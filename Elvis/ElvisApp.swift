14a812b1e492a7eeed124375542fb97572295788
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-09 18:26:28 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Theodore Utomo on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElvisTests/ElvisTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisTests.swift
+//  ElvisTests
+//
+//  Created by Theodore Utomo on 9/9/24.
+//
+
+import Testing
+@testable import Elvis
+
+struct ElvisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
